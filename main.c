63be098d5682de469168c4180575b68ed0a444e8
sunshineduck <164961202+sunshineduck@users.noreply.github.com>
2025-08-12 09:46:37 -0400

    don

--- main.c
+++ main.c
@@ -8,15 +8,14 @@
 #include <stdbool.h>
 
 
-=======
-#include <stdio.h>
-#include <stdint.h>
-#include <stddef.h>
-#include <assert.h>
-#include <avr/io.h>
-#include <stdlib.h>
-#include <stdbool.h>
+#include "relay.h"
+#include "soil.h"
 
-DDRB = DDRB|(0<<8);
->>>>>>> a8b9e64d100345cb4de584ca01a0edf612bd0563
+int main(){
+    relay_init();
+    reading_value();
+    
+}
 
+
+  
